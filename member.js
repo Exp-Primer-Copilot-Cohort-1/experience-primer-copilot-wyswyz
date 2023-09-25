aa806843a8d46ab3d8e2e6e51d3760b97447cd80
Yongshun Wang <93724323+wyswyz@users.noreply.github.com>
2023-09-25 13:45:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,61 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    var member2 = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    var member3 = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    var member4 = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    var member5 = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    var member6 = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    var member7 = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    var member8 = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+    var member9 = {
+        name: "John",
+        age: 30,
+        skills: ["JavaScript", "HTML", "CSS"],
+        salary: 4000
+    };
+
+    var members = [member, member2, member3, member4, member5, member6, member7, member8, member9];
+    for (var i = 0; i < members.length; i++) {
+        console.log(members[i]);
+    }
+}
